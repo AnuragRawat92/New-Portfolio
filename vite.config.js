361750ccd51f0cf3360091ce4280bdf522929d3f
AnuragRawat92 <148265902+AnuragRawat92@users.noreply.github.com>
2025-06-17 22:54:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/anurag-portfolio/", // ✅ trailing slash is optional but preferred
-  plugins: [react()],
+ // ✅ trailing slash is optional but preferred
+  plugins: [react(),  tailwindcss()],
 });
